7a38b04188158485b4f451937bd86a4ac47d2232
Javad <com.javadabl@gmail.com>
2025-02-19 22:42:21 +0330

    C

---
+++ CarStoreApp.Server/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CarStoreApp.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarStoreApp.Server.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public  DbSet<User> Users { get; set; }
+    }
+}
--- CarStoreApp.Server/Program.cs
+++ CarStoreApp.Server/Program.cs
@@ -1,6 +1,13 @@
+using CarStoreApp.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(op =>
+{
+    op.UseSqlite(builder.Configuration.GetConnectionString("cs"));
+}, ServiceLifetime.Singleton);
 
 builder.Services.AddControllers();
 
