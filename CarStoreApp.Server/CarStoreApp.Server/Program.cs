cd43d1686b31e4f3bfecbe2d42561761a154bfaa
Javad <com.javadabl@gmail.com>
2025-03-01 23:21:33 +0330

    m

--- CarStoreApp.Server/CarStoreApp.Server/Program.cs
+++ CarStoreApp.Server/CarStoreApp.Server/Program.cs
@@ -1,8 +1,11 @@
 using CarStoreApp.Server.Data;
+using CarStoreApp.Server.Interfaces;
+using CarStoreApp.Server.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddDbContext<AppDBContext>(op =>
 {
     op.UseSqlite(builder.Configuration.GetConnectionString("main"));
